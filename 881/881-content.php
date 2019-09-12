d85a573f201413246befa667078340a48560e4cf
Quý <toxuanquy@users.noreply.github.com>
2019-09-12 11:03:06 +0700

    12/9/2019 fix nav menu 881-content.php

--- 881/881-content.php
+++ 881/881-content.php
@@ -11,17 +11,16 @@ $url_path = str_replace('\\', '/', $url_path);
     <div class="container">
         <div class="row">
             <div class="megamenu">
-
                 <nav class="navbar navbar-inverse">
                     <div class="navbar-header">
                         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <h2 class="mb-title">Cosmetorix</h2>
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <h2 class="mb-title">Cosmetorix</h2>
+                        <!-- <h2 class="mb-title">Cosmetorix</h2> -->
                     </div>
-
                     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                         <ul class="nav navbar-nav">
                             <li class="mega-dropdown">
